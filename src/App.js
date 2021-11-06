b42624bab3390f1023de92b1523a385a8334d397
azi026 <a.robatjazi560@gmail.com>
2021-11-06 09:21:18 +0330

    p

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { Component } from 'react';
+import './App.css';
+import Navbar from './component/Navbar';
+import {BrowserRouter as Router, Switch,Route} from "react-router-dom";
+import Reports from './pages/Reports';
+import Product from './pages/Products';
+import Home from './pages/Home';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+    <Router>
+       <Navbar />
+       <Switch>
+         <Route path='/' exact component={Home}/>
+         <Route path='/reports' component={Reports}/>
+         <Route path='/products' component={Product}/>        
+       </Switch>
+     </Router> 
+  
+    </>
   );
 }
 
